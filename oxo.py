7550f5c9edd2f80e0b0809ce047d345c19bb2375
Startrails <matthewtaylor2427@gmail.com>
2018-03-22 04:07:58 +0000

    completed up to the get winner segment honestly no clue so far on how to implement that and instead of looking up other pieces of work for it i would prefer to review tutorials on coding this game as i find step by step things like that easier

--- oxo.py
+++ oxo.py
@@ -1,21 +1,32 @@
 class OxoBoard:
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        self.board =[[0,0,0],
+                     [0,0,0],
+                     [0,0,0]]
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        return self.board[x][y]
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        if self.board[x][y] == 0:
+            self.board[x][y] = mark
+            return True
+        else:
+            return False
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+        for y in xrange(0, self.row):
+            for x in xrange(0, self.col):
+                 if self.board[x][y] == 0:
+                     return False
+
+        return True
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
